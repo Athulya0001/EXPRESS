2c3e11575bcd57a6ce1dc83a1f55824ceca5f8d3
athu <athulyas2024@gmail.com>
2025-01-06 23:29:20 +0530

    updated server

--- public/script.js
+++ public/script.js
@@ -2,16 +2,23 @@ async function handleSubmit() {
     const name = document.getElementById('name').value;
     console.log(name)
 
-    async function fetchData(){
-        console.log('function called')
-        const response = await fetch('http://localhost:3000/add', {
-            method: 'POST'
-        });
-        console.log(response,"response")
-        const data = await response.json();
-        console.log(data, data)
-    }
-    fetchData();
+    if (!name) {
+        console.error('Name is required');
+        return;
+      }
 
-}
+    const response = await fetch('http://localhost:3000/add',{
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({name})
+    });
+
+    const data = await response.json();
+    console.log(data, "response after adding data");
+
+    document.getElementById('name').value = "";
+
+};
+
+
 
--- server.js
+++ server.js
@@ -4,24 +4,40 @@ import mongoose from 'mongoose';
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
 
-const url = 'mongodb://127.0.0.1:27017';
+const url = 'mongodb://127.0.0.1:27017/userData';
 mongoose.connect(url).then(()=>{
     console.log('Connected to MongoDB');
     app.listen(3000,()=>{
         console.log('Server is running on port 3000');
-    })
-}).catch(()=>{
-    console.log('Failed to connect to MongoDB');
+    });
+}).catch((err)=>{
+    console.log('Failed to connect to MongoDB', err);
 })
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+  });
+  
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res)=>{
     res.sendFile('index.html')
     console.log(res, 'response')
 })
-app.post('/add', (req, res)=>{
-    res.send('data send')
+app.post('/add', async (req, res)=>{
+    const {name} = req.body;
     console.log(req.body,"req body")
+
+    if (!name) {
+        return res.status(400).json({ error: 'Name is required' });
+      }
+
+    const user = new User({ name });
+    const savedUser =await user.save();
+    console.log(savedUser,"saveduser");
+    console.log(JSON.parse(savedUser),"parsed value");
 })
 
 app.get('/todo',(req, res)=>{
