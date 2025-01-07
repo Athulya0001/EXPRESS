8c8a0552f15949115574d753229f1b864ac827a4
athu <athulyas2024@gmail.com>
2025-01-07 15:46:38 +0530

    created database and added data to the db

--- public/script.js
+++ public/script.js
@@ -1,24 +1,37 @@
 async function handleSubmit() {
-    const name = document.getElementById('name').value;
-    console.log(name)
+  const name = document.getElementById("name");
+  console.log(name.value);
 
-    if (!name) {
-        console.error('Name is required');
-        return;
-      }
+  if (!name) {
+    console.error("Name is required");
+    return;
+  }
 
-    const response = await fetch('http://localhost:3000/add',{
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({name})
-    });
+  const response = await fetch("http://localhost:3000/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name }),
+  });
 
-    const data = await response.json();
-    console.log(data, "response after adding data");
+  const data = await response.json();
+  console.log(data, "response after adding data");
 
-    document.getElementById('name').value = "";
+  document.getElementById("name").value = "";
+}
 
-};
+async function fetchData() {
+  const response = await fetch("http://localhost:3000/getData", {
+    method: "GET",
+  });
+  const data = await response.json();
+  console.log(data);
 
+  const list = document.getElementById("list");
+  list.innerHTML = "";
 
+  const listCreate = document.createElement('li');
+  listCreate.textContent = data;
+
+  list.appendChild(listCreate);
+}
 
--- server.js
+++ server.js
@@ -1,45 +1,57 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-const url = 'mongodb://127.0.0.1:27017/userData';
-mongoose.connect(url).then(()=>{
-    console.log('Connected to MongoDB');
-    app.listen(3000,()=>{
-        console.log('Server is running on port 3000');
+const url = "mongodb://127.0.0.1:27017/userData";
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
     });
-}).catch((err)=>{
-    console.log('Failed to connect to MongoDB', err);
-})
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+  name: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+app.post("/add", async (req, res) => {
+
+
+    const data = req.body;
+  console.log(req.body, "req body");
+  const user = new User(data);
+  await user.save();
+  res.send("User added successfully");
+
+
+})
+
+app.get("/getData", (req, res) => {
+
+
+  const users = User.find().then((user)=>{
+    res.json(users);
+
+    console.log(users,"users")
+
   });
-  
-const User = mongoose.model('User', userSchema);
 
-app.get('/', (req, res)=>{
-    res.sendFile('index.html')
-    console.log(res, 'response')
-})
-app.post('/add', async (req, res)=>{
-    const {name} = req.body;
-    console.log(req.body,"req body")
+});
 
-    if (!name) {
-        return res.status(400).json({ error: 'Name is required' });
-      }
-
-    const user = new User({ name });
-    const savedUser =await user.save();
-    console.log(savedUser,"saveduser");
-    console.log(JSON.parse(savedUser),"parsed value");
-})
-
-app.get('/todo',(req, res)=>{
-    res.send('rendered add path')
-})
+app.get("/todo", (req, res) => {
+  res.send("rendered add path");
+});
